558202fd586393a551e51bc3085932cf08a37ad6
Pudjo Isnanto <pudjoisnanto@gmail.com>
2022-06-11 14:12:26 +0700

    Basket Mivroservices developed

---
+++ src/Services/Bucket/BucketAPI/Repositories/IBasketRepository.cs
@@ -0,0 +1,10 @@
+﻿using BucketAPI.Entities;
+
+namespace BucketAPI.Repositories;
+
+public interface IBasketRepository
+{
+    Task<ShoppingCart> GetBasket(string userName);
+    Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+    Task DeleteBasket(string userName);
+}
